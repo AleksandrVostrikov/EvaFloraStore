d428973ceb1c3c75ba9a75d30ea4852c629b4d17
AleksandrVostrikov <vostrik-aleksandr@yandex.ru>
2023-04-07 23:29:44 +0300

    07.04

---
+++ EvaFloraStore/Data/ItemsDbContext.cs
@@ -0,0 +1,14 @@
+﻿using EvaFloraStore.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EvaFloraStore.Data
+{
+    public class ItemsDbContext : DbContext 
+    {
+        public ItemsDbContext(DbContextOptions<ItemsDbContext> options) : base(options) { }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+
+    }
+}
---
+++ EvaFloraStore/Models/Product.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EvaFloraStore.Models
+{
+    public class Product
+    {
+        public Guid Id { get; set; }
+        [Required(ErrorMessage ="Укажите название товара")]
+        public string Name { get; set; }
+        [Required(ErrorMessage ="Добавьте описание товара")]
+        public string Description { get; set; }
+
+        [Required(ErrorMessage ="Выберите категорию")]
+        
+        [ForeignKey("CategoryId")]
+        public Category Category { get; set; }
+
+        public Guid CategoryId { get; set; }
+
+        [Required]
+        [Range(0.01, double.MaxValue, ErrorMessage = "Цена должна быть больше нуля")]
+        [Column(TypeName = "decimal(8, 2)")]
+        public decimal Price { get; set; }
+    }
+}
